aa431baa56cbbcca1b089b14dc032d99f2bb7b59
e1b18051 <e1b18051@st.oit.ac.jp>
2020-10-13 15:34:58 +0900

    初期化

---
+++ src/main/java/oit/is/lec3_6/login3_6/Login36Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec3_6.login3_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login36Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Login36Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec3_6/login3_6/Login36ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec3_6.login3_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Login36ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
